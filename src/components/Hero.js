ff3e230035d7721f29b58d2b886a8fcc69904034
Bemnet Dejene <bdejene19@gmail.com>
2021-10-20 09:30:14 -0400

    fixed collapsability of hero section. NOTE: when working with figma, need to figure out how to make svgs for easily responsive when being set as a bg image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
                 <h1>Who am I</h1>
                 <p>A self driven Software Engineer focusing on the JavaScript stacks like React for Front End Development. Also experienced in Python OOP data analytics. </p>
             </div>
-            {/* <img src={process.env.PUBLIC_URL + '/desktopContent/herobackground.svg'} alt='hero background' className='bg-photo' id='desktop-bg'></img> */}
+            <img src={process.env.PUBLIC_URL + '/desktopContent/herobackground.svg'} alt='hero background' className='bg-photo' id='desktop-bg'></img>
             <img src={process.env.PUBLIC_URL + '/tabletContent/resized-ipadMini-bg.svg'} alt='hero background' className='bg-photo' id='ipadmini-bg'></img>
             <img src={process.env.PUBLIC_URL + '/iphoneContent/iphoneX-bg.svg'} alt='iphone BG background' className='bg-photo' id='iphoneX-bg'></img>
 
@@ -24,7 +24,7 @@ export default function Hero() {
 
 
 const HeroWrapper = styled.section`
-    height: 85vh;
+    height: 87vh;
     display: grid;
     grid-template-columns: 3fr 1fr;
     overflow: hidden;
@@ -60,10 +60,11 @@ const HeroWrapper = styled.section`
         }
     }
 
-    #ipadmini-bg, #iphoneX-bg {
+    #desktop-bg, #ipadmini-bg, #iphoneX-bg {
         display: none;
     }
 
+    /* for xl screens */
     @media screen and (min-width: 2000px) {
         .intro-text {
             h1 {
@@ -75,8 +76,29 @@ const HeroWrapper = styled.section`
         }
     }
 
+    /* specific edge case for ipad pro tablets */
+    @media screen and (max-width: 1024px) and (min-height: 1200px) {
+        height: 40vh;   
+        #desktop-bg {
+            display: block;
+        }
+
+        .quick-contact {
+            top: 0;
+            .contact-icon {
+                height: 10vh;
+            }
+
+            #linkedin-icon {
+                height: 8.5vh;
+            }
+        }
+        
+        background-image: none;
+    }
+
     @media screen and (max-width: 1200px) {
-            .intro-text {
+        .intro-text {
             /* padding-top: 3em; */
             padding-left: 2.25em;
 
@@ -86,7 +108,7 @@ const HeroWrapper = styled.section`
             }
         }
         .quick-contact {
-            margin-top: 20%;
+            margin-top: -10em;
             .contact-icon {
                 width: minmax(4vw, 7vw);
             }
@@ -100,9 +122,9 @@ const HeroWrapper = styled.section`
         .quick-contact{
             top: 0;
         }
-        #desktop-bg {
+        /* #desktop-bg {
             display: none;
-        }
+        } */
 
         #ipadmini-bg {
             display: block;
@@ -137,7 +159,7 @@ const HeroWrapper = styled.section`
             
 
             p {
-                width: 60%;
+                width: 55%;
                 padding-top: 2em;
             }
         }
@@ -150,9 +172,8 @@ const HeroWrapper = styled.section`
 
     @media screen and (max-width: 600px) {
         .intro-text {
-            margin-top: 35%;
+            margin-top: 40%;
             padding-left: 1em;
-            padding-top: 1em;
 
             h1 {
                 font-size: 2.5rem;
@@ -162,7 +183,7 @@ const HeroWrapper = styled.section`
                 font-size: 20px;
                 min-width: 40%;
                 max-width: 55%;
-                padding-top: 3.5em;
+                padding-top: 4em;
             }
         }
 
@@ -204,11 +225,15 @@ const HeroWrapper = styled.section`
         .quick-contact {
             margin-left: 10%;
             left:0;
-            top: 75%;
+            top: 80%;
             column-gap: 0;
 
-            .contact-icon, #linkedin-icon {
-                width: 15vw;
+            .contact-icon {
+                height: 10vh;
+            }
+            #linkedin-icon {
+                margin-top: 0.4em;
+                height: 8.5vh;
             }
         }
     }
