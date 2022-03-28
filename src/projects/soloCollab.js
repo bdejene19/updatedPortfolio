694c715dcf688b6697b69209c68a32aa1067148a
Bemnet Dejene <bdejene19@gmail.com>
2022-03-27 22:09:05 -0400

    updated structure of projects => created own folder

---
+++ src/projects/soloCollab.js
@@ -0,0 +1,43 @@
+export const soloCollab = [
+  {
+    name: "Universal Movies",
+    technologies: ["React", "Material-UI", "JS", "CSS"],
+    description:
+      "See the latest and top rated movies, providing user's the ability to search for movies and watch movie trailer.",
+    newSkills: ["Tailwind", "jQuery", "IMDb/TMDb APIs"],
+    howToImprove: [
+      "Create genres page for increased user filtration",
+      "Improved performance for speed of trailer modal open",
+    ],
+    projectDisplay: "marvelousMovies",
+    sourceCode: "https://github.com/bdejene19/MarvelousMoviesIndex",
+    liveVersion: "https://bdejene19.github.io/MarvelousMoviesIndex/",
+  },
+  {
+    name: "Hive Mind",
+    technologies: [
+      "Express",
+      "MySQL2",
+      "bcrypt",
+      "Express-Session",
+      "JS",
+      "HTML Templating Engine (Handlebars)",
+      "Moment.js/Anime.js/Darkmode.js",
+      "Heroku",
+    ],
+    description:
+      "Join the Hive Mind and add a personal note on how to fight or persist through mental illness. Create, delete and upvote posts in the community, while sensitive information in the database is stored securely using bcrypt. Also offers session expiration, reducing number of sign in's to increase user satisfaction",
+    newSkills: [
+      "Sequlize Object Relational Mapping",
+      "Heroku deploymen using JawsDB for MySQL2 integration",
+      "Sequlize database hooks and methods",
+    ],
+    howToImprove: [
+      "Add update functionality to previous thoughts",
+      "Include moderator to main integrity of application - bolster mental health.",
+    ],
+    projectDisplay: "",
+    sourceCode: "https://github.com/bdejene19/Hive-Mind",
+    liveVersion: "https://hivemind-554.herokuapp.com/",
+  },
+];
